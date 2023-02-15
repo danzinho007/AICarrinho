7d43aa4c2e3558a8158055f1b1249a03f48095c4
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-15 10:06:52 -0200

    Create Rastros.cs

---
+++ Rastros.cs
@@ -0,0 +1,10 @@
+
+namespace ProjetoPista
+{
+    public class Rastro
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int distancia { get; set; }
+    }
+}
